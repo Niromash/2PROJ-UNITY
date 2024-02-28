5d998e490d2d4085581f4b611a6a4be93aa90ea0
Thiméo <thimeo.ballesta@supinfo.com>
2024-02-28 21:56:12 +0100

    update spawn of 2 entities

--- Assets/Scripts/SpawnEntity.cs
+++ Assets/Scripts/SpawnEntity.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class SpawnEntity : MonoBehaviour
 {
-
     public GameManager gameManager;
-    public GameObject spawnObjectPrefab; 
+    public GameObject infantryPrefab;
+    public GameObject antiArmorPrefab;
     public Vector2 spawnPosition;
 
-    public void Click()
+
+    public void InfantrySpawn()
     {
-        GameObject spawnedObject = Instantiate(spawnObjectPrefab, spawnPosition, Quaternion.identity);
+        Debug.Log("Infantry Spawn");
+        Spawn(infantryPrefab);
+    }
+
+    public void AntiArmorSpawn()
+    {
+        Debug.Log("AntiArmor Spawn");
+        Spawn(antiArmorPrefab);
+    }
+
+    private void Spawn(GameObject prefab)
+    {
+        GameObject spawnedObject = Instantiate(prefab, spawnPosition, Quaternion.identity);
         spawnedObject.SetActive(true);
         gameManager.AddEntity(spawnedObject);
     }

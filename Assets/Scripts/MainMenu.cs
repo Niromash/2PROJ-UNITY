5964ad23ac63ad59de9f57ed36a04af41a8b99e0
lucas <lucas.moreau@supinfo.com>
2024-03-28 14:57:20 +0100

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void LoadSinglePlayer()
+    { 
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    
+}

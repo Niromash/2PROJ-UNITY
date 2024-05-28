fa1679b8b51a5b561750aaf936a9cf9f59fbff84
Mattéo <niromash37@gmail.com>
2024-05-28 09:38:27 +0200

    fix spells collisions

--- Assets/Scripts/spells/SpellCollisionController.cs
+++ Assets/Scripts/spells/SpellCollisionController.cs
@@ -4,15 +4,16 @@ public class SpellCollisionController : MonoBehaviour
 {
     public GameManager gameManager;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Using OnTriggerEnter2D instead of OnCollisionEnter2D to detect collision without impacting the physics
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         Spell spell = gameManager.GetSpell(gameObject);
         if (spell == null) return;
 
         // Vérifiez si l'objet entrant en collision est le sol
-        if (gameObject.transform.position.y <= 0 || collision.gameObject != null)
+        if (gameObject.transform.position.y <= 0 || collider.gameObject != null)
         {
-            Spell collidedSpell = gameManager.GetSpell(collision.gameObject);
+            Spell collidedSpell = gameManager.GetSpell(collider.gameObject);
             if (collidedSpell == null)
             {
                 // Détruisez l'objet
@@ -21,7 +22,7 @@ public class SpellCollisionController : MonoBehaviour
         }
 
         // Vérifiez si l'objet entrant en collision est un ennemi
-        GameObject collidedObject = collision.gameObject;
+        GameObject collidedObject = collider.gameObject;
         if (collidedObject.CompareTag("Template")) return;
         Entity collidedEntity = gameManager.GetEntity(collidedObject);
         if (collidedEntity == null) return;
